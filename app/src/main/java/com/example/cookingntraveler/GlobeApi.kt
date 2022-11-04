9f3aff87738e4746ac4e7b86825f9b7c215397ed
Shiyu Xu <shiyuxu001@gmail.com>
2022-11-03 19:27:03 -0500

    first commit

---
+++ app/src/main/java/com/example/cookingntraveler/GlobeApi.kt
@@ -0,0 +1,4 @@
+package com.example.cookingntraveler
+
+class GlobeApi {
+}
---
+++ app/src/main/java/com/example/cookingntraveler/RecipesFragment.kt
@@ -0,0 +1,44 @@
+package com.example.cookingntraveler
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.cookingntraveler.databinding.RecipesFragmentBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class RecipesFragment : Fragment() {
+
+    private var _binding: RecipesFragmentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = RecipesFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
