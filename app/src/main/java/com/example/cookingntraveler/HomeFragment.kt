fb429f47ebeb9290f01cbfde7a505c476014071d
anniethepro <anniethepro@gmail.com>
2022-11-22 13:50:01 -0600

    google maps moved to fragment

--- app/src/main/java/com/example/cookingntraveler/HomeFragment.kt
+++ app/src/main/java/com/example/cookingntraveler/HomeFragment.kt
@@ -1,174 +1,77 @@
 package com.example.cookingntraveler
 
-package edu.cs371m.fcgooglemaps
-
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.Geocoder
+import android.R
 import android.os.Bundle
-import android.util.Log
-import android.view.KeyEvent
-import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.cookingntraveler.databinding.FragmentMapBinding
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import edu.cs371m.fcgooglemaps.databinding.ActivityMainBinding
-import edu.cs371m.fcgooglemaps.databinding.ContentMainBinding
-import java.util.*
 
 
-class HomeFragment
-    : AppCompatActivity(),
-    OnMapReadyCallback
-{
-    private lateinit var map: GoogleMap
-    private lateinit var geocoder: Geocoder
-    private var locationPermissionGranted = false
-    private lateinit var binding: ContentMainBinding
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(activityMainBinding.root)
-        setSupportActionBar(activityMainBinding.toolbar)
-        binding = activityMainBinding.contentMain
-
-        checkGooglePlayServices()
-        requestPermission()
-        // XXX Write me.
-
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.mapFrag) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
-//        (supportFragmentManager.findFragmentById(R.id.mapFrag) as SupportMapFragment?)?.let {
-//            it.getMapAsync(this)
-//        }
-
-
-        //init Geocode object
-        geocoder = Geocoder(this)
-
-        // This code is correct, but it assumes an EditText in your layout
-        // called mapET and a go button called goBut
-        binding.mapET.setOnEditorActionListener { /*v*/_, actionId, event ->
-            // If user has pressed enter, or if they hit the soft keyboard "send" button
-            // (which sends DONE because of the XML)
-            if ((event != null
-                        &&(event.action == KeyEvent.ACTION_DOWN)
-                        &&(event.keyCode == KeyEvent.KEYCODE_ENTER))
-                || (actionId == EditorInfo.IME_ACTION_DONE)) {
-                hideKeyboard()
-                binding.goBut.callOnClick()
-            }
-            false
-        }
-        binding.goBut.setOnClickListener{
-            //on go, read the EditText box and geocode that address to get a lat/long pair
-            //if the geocoding succeeds, use the first suggested address
-            val addr = binding.mapET.text.toString()
-            val loc = geocoder.getFromLocationName(addr, 1)[0]
-            //move the map’s camera to that location at zoom level 15.0f.
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(loc.latitude, loc.longitude), 15.0f))
-        }
-
-    }
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        if( locationPermissionGranted ) {
-            // XXX Write me.
-            // Note, we checked location permissions in requestPermission, but the compiler
-            // might complain about our not checking it.
-//
-            map.isMyLocationEnabled = true
-            map.uiSettings.isMyLocationButtonEnabled = true
-
-            map.setOnMapLongClickListener {
-                // clear markers
-                map.clear()
-            }
-
-            binding.cancelButton.setOnClickListener {
-                map.clear()
-            }
-
-            map.setOnMapClickListener {
-                //set marker at location
-                val roundLat = String.format("%.3f", it.latitude)
-                val roundLon = String.format("%.3f", it.longitude)
-                val finalStr = String.format("$roundLat,$roundLon")
-                map.addMarker(
-                    MarkerOptions()
-                        .position(it)
-                        .title(finalStr)
-                )
-            }
-        }
-
-        // XXX Write me.
-        // Start the map at the Harry Ransom center
-        val harryLoc = geocoder.getFromLocationName("Harry Ransom Center",1)[0]
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(harryLoc.latitude,harryLoc.longitude),15.0f))
-    }
-
-    // Everything below here is correct
-
-    // An Android nightmare
-    // https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
-    // https://stackoverflow.com/questions/7789514/how-to-get-activitys-windowtoken-without-view
-    private fun hideKeyboard() {
-        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(window.decorView.rootView.windowToken, 0);
-    }
-
-    private fun checkGooglePlayServices() {
-        val googleApiAvailability = GoogleApiAvailability.getInstance()
-        val resultCode =
-            googleApiAvailability.isGooglePlayServicesAvailable(this)
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (googleApiAvailability.isUserResolvableError(resultCode)) {
-                googleApiAvailability.getErrorDialog(this, resultCode, 257)?.show()
-            } else {
-                Log.i(javaClass.simpleName,
-                    "This device must install Google Play Services.")
-                finish()
-            }
+    companion object {
+        const val titleKey = "mapTitle"
+        fun newInstance(title: String): HomeFragment {
+            val frag = HomeFragment()
+            val bundle = Bundle()
+            bundle.putString(titleKey, title)
+            frag.arguments = bundle
+            return frag
         }
     }
+    private var _binding: FragmentMapBinding? = null
 
-    private fun requestPermission() {
-        val locationPermissionRequest = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permissions ->
-            when {
-                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                    locationPermissionGranted = true;
-                } else -> {
-                Toast.makeText(this,
-                    "Unable to show location - permission required",
-                    Toast.LENGTH_LONG).show()
-            }
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+
+        val supportMapFragment =
+            childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment? // TODO: resolve
+
+        // Async map
+        supportMapFragment!!.getMapAsync { googleMap ->
+            // When map is loaded
+            googleMap.setOnMapClickListener { latLng -> // When clicked on map
+                // Initialize marker options
+                val markerOptions = MarkerOptions()
+                // Set position of marker
+                markerOptions.position(latLng)
+                // Set title of marker
+                markerOptions.title(latLng.latitude.toString() + " : " + latLng.longitude)
+                // Remove all marker
+                googleMap.clear()
+                // Animating to zoom the marker
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                // Add marker on map
+                googleMap.addMarker(markerOptions)
             }
         }
-        locationPermissionRequest.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
-
